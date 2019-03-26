9075231568109f90c28f5932493d08c0deeecefe
felix0080 <642664360@qq.com>
2019-03-26 09:37:33 +0800

    py

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
 from bcc import BPF
-# define BPF program (""" is used for multi-line string).
-# '#' indicates comments for python, while '//' indicates comments for C.
 prog = """
 #include <uapi/linux/ptrace.h>
 #include <uapi/linux/limits.h>
@@ -27,26 +25,18 @@ int hello(struct pt_regs *ctx, int dfd, const char __user *filename, int flags){
     return 0;
 }
 """
-# load BPF program
 b = BPF(text=prog)
-# attach the kprobe for do_sys_open, and set handler to hello
 b.attach_kprobe(event="do_sys_open", fn_name="hello")
-# process event
 start = 0
 def print_event(cpu, data, size):
     global start
-    # event’s type is data_t
     event = b["events"].event(data)
     if start == 0:
             start = event.ts
     time_s = (float(event.ts - start)) / 1000000000
     print("%-18.9f %-16s %-6d %-16s" % (time_s, event.comm, event.pid, event.fname))
-
-# loop with callback to print_event
 b["events"].open_perf_buffer(print_event)
-# print header
 print("%-18s %-16s %-6s %-16s" % ("TIME(s)", "COMM", "PID", "FILE"))
-# start the event polling loop
 while 1:
     try:
         b.perf_buffer_poll()
